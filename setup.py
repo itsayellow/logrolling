1bd9650b5e9fd19a287d2e65eae1c7ced38d1b1d
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:36:53 -0800

    Add url, reformat via black.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-        name='logrolling',
-        version='0.1',
-        description='A wrapper for logging to make it easier and more pythonic.',
-        author='Matthew A. Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        py_modules=['logrolling'],
-        install_requires=[
-            ],
-        python_requires='>=3',
-        )
+    name="logrolling",
+    version="0.1",
+    description="A wrapper for logging to make it easier and more pythonic.",
+    author="Matthew A. Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    url="https://github.com/itsayellow/logrolling",
+    py_modules=["logrolling"],
+    install_requires=[],
+    python_requires=">=3",
+)
